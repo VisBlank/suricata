9d0c6c4db65be782416c813cf2a81a588d2556e5
coanor <coanor@gmail.com>
2014-03-26 11:30:59 +0800

    modified:   Makefile 	modified:   cfg.sh 	modified:   contrib/Makefile 	modified:   contrib/file_processor/Action/Makefile 	modified:   contrib/file_processor/Makefile 	modified:   contrib/file_processor/Processor/Makefile 	modified:   contrib/tile_pcie_logd/Makefile 	modified:   doc/Makefile 	modified:   rules/Makefile 	modified:   src/Makefile 	modified:   src/build-info.h

--- cfg.sh
+++ cfg.sh
@@ -9,5 +9,8 @@
 	--enable-non-bundled-htp \
 	--with-libhtp-includes=/usr/local/include/htp \
 	--with-libhtp-libraries=/usr/local/lib \
+	--with-libjansson-includes=/usr/local/include \
+	--with-libjansson-libraries=/usr/local/lib \
     CFLAGS="-O0 -g -g3" \
+
 	#--enable-unittests 
